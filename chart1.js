29687aec2b1b39be8e5dd7d43874de4cfbe2c2c5
Leofer Dela Cruz <Leoferma099@gmail.com>
2024-04-01 16:58:25 +0800

    Addition Chart Js

--- chart1.js
+++ chart1.js
@@ -1,11 +1,32 @@
-const ctx = document.getElementById('myChart');
+const ctx = document.getElementById('barChart');
       
 new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: 'ALL CATEGORY',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
+const ctx2 = document.getElementById('donutChart');
+      
+new Chart(ctx2, {
   type: 'doughnut',
   data: {
     labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
     datasets: [{
-      label: '# of Votes',
+      label: 'ALL CATEGORY',
       data: [12, 19, 3, 5, 2, 3],
       borderWidth: 1
     }]
