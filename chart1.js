7bebbf756e987ff3995dbd380cd09322c6ab257b
Leofer Dela Cruz <Leoferma099@gmail.com>
2024-04-08 17:04:28 +0800

    Modification     -   Build a sidebar function which displaying block and none     -   Finalize creating dashboard panel theme

--- chart1.js
+++ chart1.js
@@ -1,24 +1,3 @@
-const ctx = document.getElementById('barChart');
-      
-new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-      label: 'ALL CATEGORY',
-      data: [12, 19, 3, 5, 2, 3],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    }
-  }
-});
-
 const ctx2 = document.getElementById('donutChart');
       
 new Chart(ctx2, {
